ee9d78bad217715af7a4ee90962d8f15b49ce85e
Santiago Franco Zapata <santiagofrancozapata5@gmail.com>
2022-10-16 00:21:03 -0500

    css contacto

--- src/components/js/Contact.js
+++ src/components/js/Contact.js
@@ -1,19 +1,31 @@
-import React, { Fragment } from 'react'
+import styled from 'styled-components'
 
 const Contact = () => {
     return(
-        <Fragment>
-            <form id='contacto'>
-                <input                     
-                    type="text"
-                    placeholder="ingrese el nombre"
-                ></input>
-                <button
-                    type='submit'
-                >Enviar</button>
+        <>
+        <Titulo>Contacto</Titulo>
+        <div class='container'>
+            <form id='contacto' class='row g-3 mt-4 mb-5'>
+                <div classname='col-6'>
+                    <label class="form-label">Email</label>
+                    <input type="Email" class='form-control' placeholder="ingrese su Email de contacto"></input>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlTextarea1" class="form-label">Mensaje</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+                <div classname='col-6'>
+                    <button type="submit" class='btn btn-primary'>validar</button>
+                </div>
             </form>
-        </Fragment>
+        </div>
+        </>
     )
 }
+
+const Titulo = styled.h2`
+text-align: center;
+margin-top: 30px;
+`;
 
 export default Contact;
